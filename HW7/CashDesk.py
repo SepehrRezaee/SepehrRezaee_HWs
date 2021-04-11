289c4e5247b7f7b7ca05267df74d4c7f0b18f202
seperezaee <78574247+seperezaee@users.noreply.github.com>
2021-04-11 20:08:37 +0430

    Improved CashDesk class

--- HW7/CashDesk.py
+++ HW7/CashDesk.py
@@ -1,9 +1,29 @@
 class CashDesk:
-    def __init__(self, product_type, count, id_number):
-        self.id_number = id_number
-        self.count = count
+    def __init__(self, product_type, count, id_number, member_ids, fullname_members):
         self.product_type = product_type
+        self.count = count
+        self.id_number = id_number
+        self.member_ids = member_ids
+        self.fullname_members = fullname_members
         self.checker = list()
+
+    def is_a_member(self):
+        self.checker = []
+        for number in self.member_ids:
+            self.checker.append(number == self.id_number)
+        return any(self.checker)
+
+    def get_first_name(self):
+        if self.is_a_member():
+            return self.fullname_members[self.get_id_number()][0]
+        else:
+            return ""
+
+    def get_last_name(self):
+        if self.is_a_member():
+            return self.fullname_members[self.get_id_number()][1]
+        else:
+            return ""
 
     def get_product_type(self):
         return self.product_type
