c7e1fb6abb8e10131c62dc299fa2621365593af7
seperezaee <78574247+seperezaee@users.noreply.github.com>
2021-04-21 06:45:46 -0700

    Update Products.py
    Update Products class

--- HW7/Products.py
+++ HW7/Products.py
@@ -1,9 +1,14 @@
 class Products:
-    def __init__(self, product_type, product_name=0, price=0):
+    products_info = dict()
+
+    def __init__(self, product_type, product_name, price):
         self.product_type = product_type
         self.product_name = product_name
         self.price = price
         self.commission_groups = list()
+        Products.products_info[self.product_type] = {"product_name": self.product_name,
+                                                     "commission_groups": self.commission_groups,
+                                                     "price": self.price}
 
     def get_price(self):
         return self.price
@@ -19,3 +24,17 @@ class Products:
 
     def get_commission_groups(self):
         return self.commission_groups
+
+    def __repr__(self):
+        return Products.products_info
+
+
+shirt = Products("1", "shirt", 30)
+shirt.add_commission_groups("A")
+shirt.add_commission_groups("B")
+pants = Products("2", "pants", 50)
+pants.add_commission_groups("A")
+pants.add_commission_groups("C")
+shoes = Products("3", "shoes", 80)
+shoes.add_commission_groups("B")
+hat = Products("4", "hat", 20)
