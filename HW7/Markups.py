7fd9ce0773aba4cff6abee4072aeeab1a6be9763
seperezaee <78574247+seperezaee@users.noreply.github.com>
2021-04-21 06:44:57 -0700

    Update Markups.py
    Update Markup class

--- HW7/Markups.py
+++ HW7/Markups.py
@@ -1,19 +1,19 @@
 class Markup:
+    markup_info = dict()
+
     def __init__(self, product_type, lower_count, lower_cost, upper_cost):
         self.product_type = product_type
         self.lower_count = lower_count
         self.lower_cost = lower_cost
         self.upper_cost = upper_cost
-        self.count = 0
+        Markup.markup_info[self.product_type] = {"lower_count": self.lower_count,
+                                                 "lower_cost": self.lower_cost, "upper_cost": self.upper_cost}
 
     def get_product_type(self):
         return self.product_type
 
-    def get_markup_percent(self, count):
-        self.count = count
-        coefficient_markup = round(((self.lower_cost - self.upper_cost) * self.count + (
-                (self.lower_count * self.upper_cost) - self.lower_cost)) / (self.lower_count - 1), 3)
-        if self.count <= self.lower_count:
-            return coefficient_markup
-        else:
-            return self.lower_cost
+
+type_1 = Markup("1", 10, 10, 20)
+type_2 = Markup("2", 10, 15, 20)
+type_3 = Markup("3", 5, 10, 15)
+type_4 = Markup("4", 20, 10, 30)
